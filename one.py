608a0b580980ecb8f8b89dc6f8767300d90e7a75
t-aerackk <Aenjaal@gmail.com>
2023-10-04 12:28:52 +0545

    Python extended

---
+++ one.py
@@ -0,0 +1,8 @@
+p="anjal"
+b=[]
+for i in p:
+    print(i)
+    b.append(i)
+print(b)
+
+
---
+++ two.py
@@ -0,0 +1,19 @@
+#To print hello world 
+print("Hello world")
+
+#2 multi line comment
+'''print("Hello world")
+print("Hello world")
+print("Hello world")'''
+
+#Arithmetic operations
+'''23
+3000000+50000002+3
+3000000+5000000'''
+
+'''name=input("Enetr your name")
+print(f"your name is {name}")'''
+
+a=int(input("Enter any numnber\n"))
+b=int(input("enter another \n"))
+print(a+b)
